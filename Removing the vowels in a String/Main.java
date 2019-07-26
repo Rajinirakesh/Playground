43658338ec78b7e592966e82dbf1f6c68db1d9e3
Rajinirakesh <50264392+Rajinirakesh@users.noreply.github.com>
2019-07-26 12:35:22 +0530

    Test code pushed to repository.

---
+++ Removing the vowels in a String/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Main
+{
+  public static void main(String args[])
+  {
+  String str,r;
+    Scanner in=new Scanner(System.in);
+    str=in.nextLine();
+    r=removeVowels(str);
+    System.out.println( r );
+  }
+  public static String removeVowels(String s)
+  {
+    String finalString="";
+    for(int i=0; i<s.length(); i++)
+    {
+      if(!isVowel(Character.toLowerCase(s.charAt(i))))
+      {
+        finalString=finalString+s.charAt(i);
+      }
+    }
+    return finalString;
+  }
+  public static boolean isVowel(char c)
+  {
+    String vowels="aeiou";
+    for(int i=0; i<5; i++)
+    {
+      if(c == vowels.charAt(i))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  }
